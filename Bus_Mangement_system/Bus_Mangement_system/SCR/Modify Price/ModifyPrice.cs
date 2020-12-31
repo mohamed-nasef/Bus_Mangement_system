ca01904675c32947611f7394303c3f5a52241aa2
mohamed-nasef <mnasef82@gmail.com>
2020-12-31 21:31:34 +0200

    add DB in ModifyPrice Form

--- Bus_Mangement_system/Bus_Mangement_system/SCR/Modify Price/ModifyPrice.cs
+++ Bus_Mangement_system/Bus_Mangement_system/SCR/Modify Price/ModifyPrice.cs
@@ -8,11 +8,20 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Bus_Mangement_system.SCR.Modify_Price
 {
     public partial class ModifyPrice : Form
     {
+        #region DB
+
+        string conString = @"Data Source=DESKTOP-7521PNM\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";//-------
+        SqlCommand cmd;
+        SqlConnection connection = new SqlConnection();
+
+        #endregion
+
         #region Prop
 
         string strNewPrice;
@@ -26,6 +35,21 @@ namespace Bus_Mangement_system.SCR.Modify_Price
         private void ModifyPrice_Load(object sender, EventArgs e)
         {
             visible();
+            cmbBookingType.Items.Clear();
+            connection = new SqlConnection(conString);
+            connection.Open();
+            cmd = connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select booking_type_name from BOOKING_TYPE";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                cmbBookingType.Items.Add(dr["booking_type_name"].ToString());
+            }
+            connection.Close();
         }
 
         #region Close Form
@@ -99,9 +123,23 @@ namespace Bus_Mangement_system.SCR.Modify_Price
         {
             visible();
             iBookingTypeId = cmbBookingType.SelectedIndex + 1;
+
             //DB select booking type price from table where notnull
-            iOldPrice = 350;
-            txtOldPrice.Text = iOldPrice.ToString();
+            //index = cmb.SelectedIndex + 1; delete this
+
+            cmd = new SqlCommand("select booking_price from BOOKING_PRICE bp  where bp.booking_type_id ='" + iBookingTypeId + "'and bp.expiry_date is null", connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                string price = (string)dr["booking_price"].ToString();
+                txtOldPrice.Text = price;
+            }
+            connection.Close();
+
+            //iOldPrice = 350;
+            //txtOldPrice.Text = iOldPrice.ToString();
         }
 
         #endregion
@@ -117,6 +155,12 @@ namespace Bus_Mangement_system.SCR.Modify_Price
                 {
                     //DB Commands
 
+                    cmd = new SqlCommand("update BOOKING_PRICE SET expiry_date='" + DateTime.Now + "' where booking_type_id = '" + iBookingTypeId + "'", connection);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand("insert into BOOKING_PRICE (booking_type_id,booking_price)values('" + iBookingTypeId + "','" + iNewPrice + "')", connection);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
 
                     //clear
                     txtOldPrice.Clear();
