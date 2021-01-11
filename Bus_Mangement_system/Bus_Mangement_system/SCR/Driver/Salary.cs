182e0987eb71c1daf07fcff782f27420ff959ae5
moustafaibrahem <moustafa.ibrahem56@gmail.com>
2021-01-11 04:27:35 +0200

    add Query in Salary form

--- Bus_Mangement_system/Bus_Mangement_system/SCR/Driver/Salary.cs
+++ Bus_Mangement_system/Bus_Mangement_system/SCR/Driver/Salary.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,10 +15,24 @@ namespace Bus_Mangement_system.SCR.Driver
     public partial class Salary : Form
     {
 
+        #region DB
+
+        string conString = @"Data Source=DESKTOP-7521PNM\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";//-------
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+        DataRow dr;
+        DataTable dt;
+        SqlConnection connection = new SqlConnection();
+
+        #endregion
+
         #region Prop
-        int iDriverId, month;
-        int iTakenSalary, iRestOfSalary,iSalary;
-        string strSalary;
+        int iDriverId, month, year;
+        int iTakenSalary=0, iRestOfSalary=0, iSalary=0,iBasicSalary=0,total=0;
+
+        string strSalary,strBasic,strTaken;
+        DateTime date = DateTime.Now;
         #endregion
 
         public Salary()
@@ -26,11 +41,24 @@ namespace Bus_Mangement_system.SCR.Driver
         }
         private void Salary_Load(object sender, EventArgs e)
         {
-            //db select driver but in cmbDriver
+
             visible();
-            DateTime iDate = DateTime.Now;
-            month = iDate.Month;
+            month = date.Month;
+            year = date.Year;
             numMonth.Value = month;
+            cmbDriver.SelectedIndex = -1;
+            connection = new SqlConnection(conString);
+            connection.Open();
+            cmd = new SqlCommand("select driver_name from driverInformation", connection);
+            cmd.ExecuteNonQuery();
+            dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                cmbDriver.Items.Add(dr["driver_name"].ToString());
+            }
+            connection.Close();
         }
 
         #region Close Form
@@ -126,6 +154,18 @@ namespace Bus_Mangement_system.SCR.Driver
         private void CmbDriver_SelectedIndexChanged(object sender, EventArgs e)
         {
             iDriverId = cmbDriver.SelectedIndex + 1;
+            connection.Open();
+            cmd = new SqlCommand("select * from driverInformation where driver_id ='" + iDriverId + "' ", connection);
+            cmd.ExecuteNonQuery();
+            dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                strBasic = dr["basicSalary"].ToString();
+                int.TryParse(strBasic, out iBasicSalary);
+            }
+            connection.Close();
             numMon();
         }
         #endregion
@@ -143,11 +183,39 @@ namespace Bus_Mangement_system.SCR.Driver
         private void BtnSearch_Click(object sender, EventArgs e)
         {
             show();
+            iDriverId = cmbDriver.SelectedIndex + 1;
+
             //db select by iddriver get all what happen in this month in taple salary and but in iTakenSalary iRestOfSalary
-            iTakenSalary = 1900;
-            iRestOfSalary = 2100;
-            txtSalaryTaken.Text = iTakenSalary.ToString();
-            txtRestOfSalary.Text = iRestOfSalary.ToString();
+            connection = new SqlConnection(conString);
+            connection.Open();
+
+            //da = new SqlDataAdapter("SELECT * FROM driverSalary WHERE DATEPART(YEAR, monthSalary) = '" + year + "' AND DATEPART(MONTH, monthSalary) = '" + month + "'", connection);
+            cmd = new SqlCommand("SELECT * FROM driverSalary WHERE DATEPART(YEAR, monthSalary) = '" + year + "' AND DATEPART(MONTH, monthSalary) = '" + month + "' and driver_id='"+ iDriverId + "' ", connection);
+            cmd.ExecuteNonQuery();
+            try
+            {
+                dt = new DataTable();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    strTaken= dr["takenSalary"].ToString();
+                    int.TryParse(strTaken, out total);
+                    iTakenSalary += total;
+                    strBasic = dr["basicSalary"].ToString();
+                    int.TryParse(strBasic, out iBasicSalary);
+                }
+                connection.Close();
+
+                txtSalaryTaken.Text = iTakenSalary.ToString();
+                txtRestOfSalary.Text = (iBasicSalary - iTakenSalary).ToString();
+            }
+            catch (Exception)
+            {
+                txtSalaryTaken.Text = "0";
+                txtRestOfSalary.Text = iBasicSalary.ToString();
+            }
+            connection.Close();
         }
         #endregion
 
@@ -155,14 +223,24 @@ namespace Bus_Mangement_system.SCR.Driver
 
         private void BtnAddSalary_Click(object sender, EventArgs e)
         {
-            
+            int.TryParse(txtRestOfSalary.Text, out iRestOfSalary);
             if (salaryValid()&&iSalary<=iRestOfSalary)
             {
                 DialogResult result = MetroFramework.MetroMessageBox.Show(this, $"To add $ {iSalary} to {cmbDriver.SelectedValue} ", "\nAre you sure ?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (result == DialogResult.Yes)
                 {
-                    //DB Commands insert into table Bus Fees with BusId and type fees in multible row with date
+                    //DB
+                    iDriverId = cmbDriver.SelectedIndex + 1;
+                    connection.Open();
+                    cmd = new SqlCommand("insert into driverSalary(driver_id,takenSalary,basicSalary,monthSalary)values ("+iDriverId+","+iSalary+","+iBasicSalary+",'"+year+"-"+month+"-1')", connection);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
 
+                    //insert into profit
+                    connection.Open();
+                    SqlCommand cmdproce = new SqlCommand();
+                    cmdproce = new SqlCommand("exec driverTakenSalaryCheckExist '" + date + "'," + iSalary + "", connection);
+                    connection.Close();
 
 
                     //clear
