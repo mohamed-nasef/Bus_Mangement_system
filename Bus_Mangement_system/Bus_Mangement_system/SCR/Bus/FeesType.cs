b970c079ded20f11fa496a9ac42ef3b31d4b02b2
moustafaibrahem <54250605+moustafaibrahem@users.noreply.github.com>
2021-02-17 23:02:34 +0200

    Update FeesType.cs

--- Bus_Mangement_system/Bus_Mangement_system/SCR/Bus/FeesType.cs
+++ Bus_Mangement_system/Bus_Mangement_system/SCR/Bus/FeesType.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Guna.UI.WinForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,33 +14,19 @@ namespace Bus_Mangement_system.SCR.Bus
             iYear = DateTime.Now.Year;
             iMonth = DateTime.Now.Month;
             iDay = DateTime.Now.Day;
-            iChangeOil = 0;
-            iOther = 0;
-            iLicenseRenewal = 0;
-            iBusWash = 0;
-            iPeriodicMaintenance = 0;
-            iSolar = 0;
             iTotal = 0;
         }
 
+        public List<int> iPrice = new List<int>();
+        public List<string> strName = new List<string>();
+        public List<GunaButton> GunaButton = new List<GunaButton>();
+
         public int iFeesTypeID { get; set; }
-        public int iChangeOil { get; set; }
-        public int iOther { get; set; }
-        public int iLicenseRenewal { get; set; }
-        public int iBusWash { get; set; }
-        public int iPeriodicMaintenance { get; set; }
-        public int iSolar { get; set; }
         public int iTotal { get; set; }
         public int iYear { get; set; }
         public int iMonth { get; set; }
         public int iDay { get; set; }
 
-        public string strChangeOil { get; set; }
-        public string strOther { get; set; }
-        public string strLicenseRenewal { get; set; }
-        public string strBusWash { get; set; }
-        public string strPeriodicMaintenance { get; set; }
-        public string strSolar { get; set; }
         public string strMoney { get; set; }
 
 
